ce55d8903516373ce38dbba04376c9413ae7b3b6
Jeonni <jeonniu@naver.com>
2023-03-27 00:26:41 +0900

    feat: 게시글 등록 및 전체 화면 조회 기능 추가

--- src/main/java/com/testweb/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/testweb/springboot/domain/posts/PostsRepository.java
@@ -1,7 +1,12 @@
 package com.testweb.springboot.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 // DB 접근, 도메인 객체를 DB에 저장하고 관리
 public interface PostsRepository extends JpaRepository<Posts, Long> {
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
+    List<Posts> findAllDesc();
 }
--- src/main/java/com/testweb/springboot/service/posts/PostsService.java
+++ src/main/java/com/testweb/springboot/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.testweb.springboot.service.posts;
 
 import com.testweb.springboot.domain.posts.Posts;
 import com.testweb.springboot.domain.posts.PostsRepository;
+import com.testweb.springboot.web.dto.PostsListResponseDto;
 import com.testweb.springboot.web.dto.PostsResponseDto;
 import com.testweb.springboot.web.dto.PostsSaveRequestDto;
 import com.testweb.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 // 핵심 비즈니스 로직 구현
@@ -32,5 +36,12 @@ public class PostsService {
     public PostsResponseDto findById(Long id){
         Posts entity = postsRepository.findById(id).orElseThrow(()-> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
