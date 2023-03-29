5134cf8146b1149c30e6db335277d5f2f3c2a145
Jeonni <jeonniu@naver.com>
2023-03-29 15:47:43 +0900

    feat: 게시글 수정 기능 추가

--- src/main/java/com/testweb/springboot/web/IndexController.java
+++ src/main/java/com/testweb/springboot/web/IndexController.java
@@ -1,10 +1,14 @@
 package com.testweb.springboot.web;
 
 import com.testweb.springboot.service.posts.PostsService;
+import com.testweb.springboot.web.dto.PostsResponseDto;
+import com.testweb.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RequiredArgsConstructor
 @Controller
@@ -22,5 +26,13 @@ public class IndexController {
     @GetMapping("/posts/save")
     public String postsSave(){
         return "posts-save";
+    }
+
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model){
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+        return "posts-update";
     }
 }
--- src/main/java/com/testweb/springboot/web/PostsApiController.java
+++ src/main/java/com/testweb/springboot/web/PostsApiController.java
@@ -22,12 +22,14 @@ public class PostsApiController {
     }
 
     @PutMapping("/api/v1/posts/{id}")
-    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requesDto){
-        return postsService.update(id, requesDto);
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto){
+        return postsService.update(id, requestDto);
     }
 
     @GetMapping("/api/v1/posts/{id}")
     public PostsResponseDto findById(@PathVariable Long id){
         return postsService.findById(id);
     }
+
+
 }
